3d0c3e27275d118be4e76567780866fa0ad97d5a
EMSARI <e.sari@emakina.com.tr>
2020-04-05 16:18:42 +0300

    first angular project is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  passwdLength: number;
+  password: string;
+  includeLetters: boolean;
+  includeNumbers: boolean;
+  includeSymbols: boolean;
+
+  constructor() {
+    this.passwdLength = 0;
+    this.password = '';
+    this.includeLetters = false;
+    this.includeLetters = false;
+    this.includeSymbols = false;
+  }
+
+  onChangeLength( value: string ) {
+    // tslint:disable-next-line: radix
+    const parsedValue = parseInt(value);
+
+    if ( !isNaN( parsedValue ) ) {
+      this.passwdLength = parsedValue;
+    }
+  }
+
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'qwertyuıopğüasdfghjklşizxcvbnmöç';
+    const symbols = '!^+%&/()=?_-*#${[]}';
+    let charGroup: string;
+    let newPassword = '';
+
+    if ( this.includeNumbers ) { charGroup += numbers; }
+    if ( this.includeLetters ) { charGroup += letters; }
+    if ( this.includeSymbols ) { charGroup += symbols; }
+
+    for ( let i = 0; i < this.passwdLength; i++ ) {
+      const index = Math.floor( Math.random() * charGroup.length );
+      newPassword += charGroup[ index ];
+    }
+
+    this.password = newPassword;
+  }
+
+  getName() {
+    return 'Emrullah SARI';
+  }
 }
